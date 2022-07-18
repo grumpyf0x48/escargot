d00f8637d3105a8fada4881719af07908e03ce82
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2022-07-18 10:15:01 +0200

    import du projet

---
+++ src/main/java/org/grumpyf0x48/escargot/EscargotApplication.java
@@ -0,0 +1,13 @@
+package org.grumpyf0x48.escargot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscargotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscargotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/grumpyf0x48/escargot/EscargotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grumpyf0x48.escargot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscargotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
